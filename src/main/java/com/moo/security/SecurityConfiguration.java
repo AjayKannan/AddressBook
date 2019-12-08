6429d317249d6e6c4ae4da09e85e9b4123aa4efa
612082654 <ajay.kannana@bt.com>
2019-12-08 10:54:34 +0000

    Added Basic Auth

---
+++ src/main/java/com/moo/security/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.moo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER");
+	}
+}
